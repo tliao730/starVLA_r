5f1d43e3603f43eb284dcaa28fe04d1124501d81
Max Shen <maxshen1212@gmail.com>
2026-03-07 20:35:21 -0800

    Update LIBERO paths in eval scripts for consistency

--- examples/LIBERO/eval_files/eval_libero.sh
+++ examples/LIBERO/eval_files/eval_libero.sh
@@ -5,9 +5,9 @@ conda activate starVLA
 
 ###########################################################################################
 # === Please modify the following paths according to your environment ===
-export LIBERO_HOME=/mnt/petrelfs/share/yejinhui/Projects/LIBERO
+export LIBERO_HOME=/content/LIBERO
 export LIBERO_CONFIG_PATH=${LIBERO_HOME}/libero
-export LIBERO_Python=/mnt/petrelfs/share/yejinhui/Envs/miniconda3/envs/lerobot/bin/python
+export LIBERO_Python=/usr/local/envs/libero/bin/python
 
 export PYTHONPATH=$PYTHONPATH:${LIBERO_HOME} # let eval_libero find the LIBERO tools
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
@@ -16,7 +16,7 @@ export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools fro
 host="127.0.0.1"
 base_port=5694
 unnorm_key="franka"
-your_ckpt=./results/Checkpoints/1208_libero_all_QwenPI_qwen3/checkpoints/steps_50000_pytorch_model.pt
+your_ckpt=./checkpoints/steps_50000_pytorch_model.pt
 export DEBUG=true
 
 folder_name=$(echo "$your_ckpt" | awk -F'/' '{print $(NF-2)"_"$(NF-1)"_"$NF}')
--- examples/LIBERO/eval_files/run_policy_server.sh
+++ examples/LIBERO/eval_files/run_policy_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
-export star_vla_python=/mnt/petrelfs/share/yejinhui/Envs/miniconda3/envs/starVLA/bin/python
-your_ckpt=results/Checkpoints/1208_libero_all_QwenPI_qwen3/checkpoints/steps_50000_pytorch_model.pt
+export star_vla_python=/usr/local/envs/starvla/bin/python
+your_ckpt=./checkpoints/steps_50000_pytorch_model.pt
 gpu_id=7
 port=5694
 ################# star Policy Server ######################
